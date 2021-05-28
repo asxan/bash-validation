e3e5f4868a189630acfdf8b020086b916c5f5176
Vitalii Klymov <klimov09032000@gmail.com>
2021-05-28 16:33:49 +0300

    New json_parser.py script for parsing json output of xml_parser.py was written Also parser.py was renamed to xml_parser.py

---
+++ json_parser.py
@@ -0,0 +1,48 @@
+import json
+import sys
+
+
+class ArgsWrongNumber(Exception):
+    def __str__(self):
+        print("Wrong number of arguments")
+        sys.exit(1)
+
+
+def get_argument():
+    argv = sys.argv
+    try:
+        if len(argv) != 2:
+            raise ArgsWrongNumber
+        else:
+            return sys.argv
+    except ArgsWrongNumber as error:
+        print(error)
+
+
+def read_json_file(json_file):
+    with open(json_file, "r", encoding='utf-8') as file:
+        data = json.load(file)
+    return data
+
+
+def parse_json(data):
+    pretty = json.dumps(data["taskResult"]["details"], indent=4, sort_keys=True).strip("[]").strip("\n").strip("{  }")
+    return pretty
+
+
+def write_json(parse_data):
+    with open("./result_json_parse.txt", "w", encoding='utf-8') as file:
+        file.write(parse_data)
+
+
+def main():
+    arguments = get_argument()
+    data = read_json_file(arguments[1])
+    parse_data = parse_json(data)
+    write_json(parse_data)
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
