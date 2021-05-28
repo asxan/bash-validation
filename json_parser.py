e7aa9ab528981d776e14fefd830c016a608d879e
Vitalii Klymov <klimov09032000@gmail.com>
2021-05-29 00:18:33 +0300

    Some changes to arg pars function was added and also added check for file existing

--- json_parser.py
+++ json_parser.py
@@ -1,11 +1,18 @@
 import json
 import sys
+import os
 import argparse
 
 
 class ArgsWrongNumber(Exception):
     def __str__(self):
         print("Wrong number of arguments!")
+        sys.exit(1)
+
+
+class FileAbsence(Exception):
+    def __str__(self):
+        print("Can't open the file")
         sys.exit(1)
 
 
@@ -21,9 +28,15 @@ def get_argument():
 
 
 def args_parser(argv):
-    parser = argparse.ArgumentParser(description='This script parse json output of parsing xml test result')
-    parser.add_argument('json_file_path', type=str, help='json_file')
-    args = parser.parse_args(argv[1:])
+    try:
+        if len(argv) != 2:
+            raise ArgsWrongNumber
+        else:
+            parser = argparse.ArgumentParser(description='This script parse json output of parsing xml test result')
+            parser.add_argument('json_file_path', type=str, help='json_file')
+            args = parser.parse_args(argv[1:])
+    except ArgsWrongNumber as error:
+        print(error)
 
     return args
 
@@ -33,7 +46,7 @@ def read_json_file(json_file):
         with open(json_file, "r", encoding='utf-8') as file:
             data = json.load(file)
     except IOError:
-        print("File that passed as an argument has not found!")
+        print("File can't be read!")
         sys.exit(1)
     return data
 
@@ -52,8 +65,12 @@ def write_json(parse_data):
 
 
 def main():
-    arguments = get_argument()
-    parse_data = parse_json(read_json_file(arguments[1]))
+    arguments = vars(args_parser(sys.argv))
+    if arguments['json_file_path']:
+        if not os.path.exists(arguments['json_file_path']):
+            raise FileAbsence
+    json_filename = arguments['json_file_path']
+    parse_data = parse_json(read_json_file(json_filename))
     write_json(parse_data)
 
 
